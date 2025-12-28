7563756a6218aaeed602f04ef0feb5eda02931a6
Zain4382823 <zain.harith@yahoo.com>
2025-12-28 21:30:57 +0000

    Player can now catch fish!

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    bool fishingMode = false;
-    bool fishingModeCooldown = false;
+    int fishCount = 0;  // when player catches fish, we increment fish count by 1!
+
+    bool fishingMode = false;  // FISHING MODE: player casts line, waits for fish, fish bites hook, then player catches the fish!
+    bool fishingModeCooldown = false;  // prevents spamming F key, which confuses the program..
+
+    string caughtFish = "N/A";  // catching fish status, triggered when fish bites the hook. used for FishBitesTheHook() enum.
+
+    // caughtFish values and what they mean:
+
+    // ("N/A") -> Fish does not exist / player already caught fish, so it reset.
+    // ("NotCaught") -> Fish has bitten the hook, but player has not caught it yet! if player doesn't catch it in time, the fish gets away!
 
     // Start is called before the first frame update
     void Start()
     {
+        // let's just say the player's idle for now...
         Debug.Log("Currently idle..");
         Debug.Log("(Press F to enter fishing mode!)");
     }
@@ -24,7 +34,9 @@ public class Player : MonoBehaviour
             {
                 // turn fishing mode ON!!
                 fishingMode = true;
-                Debug.Log("Fishing Mode activated!");
+                Debug.Log("Casting the line..");
+                // automatically start up WaitingForFish() coroutine..
+                StartCoroutine(WaitingForFish());
                 // activate cooldown to prevent spamming + start timer to deactivate cooldown after a few seconds..
                 fishingModeCooldown = true;
                 StartCoroutine(FishModeCDTimer());
@@ -33,11 +45,22 @@ public class Player : MonoBehaviour
             {
                 // turn fishing mode OFF!!
                 fishingMode = false;
-                Debug.Log("Fishing Mode deactivated!");
+                Debug.Log("You suddenly decided to stop fishing!");
                 // activate cooldown to prevent spamming + start timer to deactivate cooldown after a few seconds..
                 fishingModeCooldown = true;
                 StartCoroutine(FishModeCDTimer());
             }
+        }
+
+        // Handle C key inputs -> Catch a fish!
+        if (Input.GetKey(KeyCode.C) && fishingMode && caughtFish == "NotCaught")  // if fishing mode is ON, a fish has bitten the hook, and player presses C..
+        {
+            // player successfully caught the fish, increment fish count by 1!
+            fishCount++;
+            Debug.Log("Congrats! You caught the fish!");
+            // turn fishing mode OFF and reset caughtFish to "N/A".
+            fishingMode = false;
+            caughtFish = "N/A";
         }
     }
 
@@ -46,5 +69,31 @@ public class Player : MonoBehaviour
     {
         yield return new WaitForSeconds(1);
         fishingModeCooldown = false;
+    }
+
+    // WAITING FOR FISH -> wait 3 seconds for a fish to bite!
+    IEnumerator WaitingForFish()
+    {
+        Debug.Log("Line casted! And now, we wait for fish..");
+        yield return new WaitForSeconds(3);
+        if(fishingMode)  // remember, we NEED to check if player is still in fishing mode!
+            StartCoroutine(FishBitTheHook());
+    }
+
+    // FISH BIT THE HOOK -> quick time event! player has a 1.5 second time window, to press C in order to catch the fish!
+    IEnumerator FishBitTheHook()
+    {
+        caughtFish = "NotCaught";  // signals to the program that a fish bit the hook, but has not been caught.
+        Debug.Log("A fish has bitten the hook!! Press C to catch the fish!");
+        
+        yield return new WaitForSeconds(1.5f);  // wait 1.5 seconds for player to catch fish..
+
+        if(caughtFish == "NotCaught" && fishingMode)  // if time runs out, and player still hasn't caught the fish..
+        {
+            Debug.Log("Oh no! The fish got away!");  // the fish gets bored and escapes! womp womp..
+            // turn fishing mode OFF and reset caughtFish to "N/A".
+            fishingMode = false;
+            caughtFish = "N/A";
+        }
     }
 }
