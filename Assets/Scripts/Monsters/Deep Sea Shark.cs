021a5e1ba4ec2b865854d340692ef6c96cd71907
Zain4382823 <zain.harith@yahoo.com>
2026-01-27 16:01:34 +0000

    Implemented Activation & ButtonMashQTE Coroutine in DeepSeaShark.cs
    WHEN WE GET BACK HOME:
    
    (1) -> REMEMBER TO DRAG ALL MONSTER SCRIPTS INSIDE OF SEA MONSTER OBJECT AS FOUR SCRIPT COMPONENTS
    
    (2) -> Drag Sea Monster object into Deep Sea Shark.cs' game object "SeaMonster" variable area.
    We have to attach it like this so that the script can activate / deactivate the sea monster game object when a monster spawns / despawns.

--- Assets/Scripts/Monsters/Deep Sea Shark.cs
+++ Assets/Scripts/Monsters/Deep Sea Shark.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class DeepSeaShark : MonoBehaviour
 {
+    // SET UP SEA MONSTER GAME OBJECT - SO THAT WE CAN ACTIVATE IT
+    public GameObject SeaMonster;
+
     // isActive values:  [0] -> Not active  [1] -> Activation triggered  [2] -> Active
     public static int isActive = 0;
 
@@ -20,11 +23,49 @@ public class DeepSeaShark : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // ACTIVATION CHECK -> Spawn monster in, set strugglepoints to 0 and start ButtonMashQTE Coroutine!
+        if(isActive == 1)
+        {
+            // activate sea monster object, make the monster visible!
+            SeaMonster.SetActive(true);
+            // reset strugglePoints back to zero!
+            strugglePoints = 0;
+            // start ButtonMashQTE coroutine!
+            StartCoroutine(ButtonMashQTE());
+            // deactivate trigger variable, while also signalling that the Deep Sea Shark is still active..
+            isActive = 2;
+        }
+
+        // LEFT MOUSE BUTTON CHECK - GRANTS STRUGGLE POINTS IF DEEP SEA SHARK IS ACTIVE
+        if(Input.GetMouseButton(0))
+        {
+            if(isActive == 2)
+                strugglePoints += 25;  // player gets 25 struggle points with each left click!
+        }
     }
 
     IEnumerator ButtonMashQTE()
     {
+        Debug.Log("DEEP SEA SHARK ATTACKS! Mash Left Mouse Button to survive!"); // ALERT THE PLAYER, TELL THEM TO MASH LEFT CLICK!
+
         yield return new WaitForSeconds(4f);
+
+        // DOES THE PLAYER HAVE MORE THAN 500 STRUGGLE POINTS??
+        
+        if (strugglePoints > 500)  // IF THEY DO...
+        {
+            // PLAYER WINS! REWARD THEM 500 GOLD AND 500 XP!
+            Progression.gold += 500;
+            Progression.XP += 500;
+            // ALSO DEACTIVATE THE DEEP SEA SHARK.
+            SeaMonster.SetActive(false);
+        }
+        else  // BUT IF THE PLAYER DOESN'T HAVE ENOUGH STRUGGLE POINTS...
+        {
+            // DEEP SEA SHARK WINS! KILL THE PLAYER!
+            Player.triggerDeath = true;
+            // ALSO DEACTIVATE THE DEEP SEA SHARK.
+            SeaMonster.SetActive(false);
+        }
     }
 }
