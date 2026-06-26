5020e830bea5dd31c87144fc54d28bb315dcdeb3
Zain4382823 <zain.harith@yahoo.com>
2026-06-27 00:25:08 +0100

    Added boxcollider, rigidbody and velocity launch for sword

--- Assets/Scripts/Monsters/Sword.cs
+++ Assets/Scripts/Monsters/Sword.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
+        // set up rb component in Start()
+        rb = GetComponent<Rigidbody2D>();
+
+        // initialise rb velocity as zero
+        rb.velocity = Vector2.zero;
+
         // start up attack delay coroutine -> wait 1.25 seconds before launching sword at player!
         StartCoroutine(AttackDelay());
     }
@@ -23,5 +31,6 @@ public class Sword : MonoBehaviour
         // wait 1.25 seconds
         yield return new WaitForSeconds(1.25f);
         // launch sword towards the player! (i'll use a velocity for this why not..)
+        rb.velocity = new Vector2(-2.5f,0f);  // LAUNCH SWORD LEFTWARDS!!
     }
 }
