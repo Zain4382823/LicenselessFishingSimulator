b65d62f2bdb8bee9cbc6bce5aeb0a50b838b1ea1
Zain4382823 <zain.harith@yahoo.com>
2026-07-13 12:01:30 +0100

    Introduced Bait.cs variables!

--- Assets/Scripts/Mechanics/Bait.cs
+++ Assets/Scripts/Mechanics/Bait.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class Bait : MonoBehaviour
 {
+    // is the menu open?
+    bool BaitMenuOpen = false;
+
+    // is a bait currently being used?
+    bool BaitActive = false;
+
+    // which option number did the player select?
+    int SelectedBaitOption = 1;
+
+    // INDIVIDUAL BAIT COUNTERS:
+    int FishBaitCount = 1; int JunkBaitCount = 1; int TreasureBaitCount = 1; int SeaMonsterBaitCount = 1; int SuperAllRounderBaitCount = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,10 @@ public class Bait : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // PRESSING B OPENS / CLOSES THE BAIT MENU!!
+        if(Input.GetKey(KeyCode.B))
+        {
+
+        }
     }
 }
