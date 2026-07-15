4e3040dbad672994c659551d5deff24b927e91b3
Zain4382823 <zain.harith@yahoo.com>
2026-07-15 12:19:15 +0100

    1-5 Option select inputs handled!

--- Assets/Scripts/Mechanics/Bait.cs
+++ Assets/Scripts/Mechanics/Bait.cs
@@ -28,21 +28,8 @@ public class Bait : MonoBehaviour
         // check if player pressed B to open / close the bait menu!
         MenuOpenInputCheck();
 
-        // INPUT CHECK 1 - Fish Bait
-        if (Input.GetKeyDown(KeyCode.Alpha1) && BaitMenuOpen)
-        {
-            if (FishBaitCount > 0)
-            {
-                // Load up UseBait Enum -> Decrement FishBaitCount -> Debug Log message confirmation
-                UseBait();
-                FishBaitCount--;
-                Debug.Log("You used a Fish Bait!");
-            }
-            else
-            {
-                Debug.Log("You don't have any fish baits!");
-            }
-        }
+        // check if player selected any number options! (1-5)
+        OptionSelectCheck();
     }
 
     IEnumerator UseBait()
@@ -69,6 +56,89 @@ public class Bait : MonoBehaviour
                 // CLOSE THE BAIT MENU!
                 Debug.Log("Bait Menu Closed!");
                 BaitMenuOpen = false;
+            }
+        }
+    }
+
+    void OptionSelectCheck()
+    {
+        // INPUT CHECK 1 - Fish Bait
+        if (Input.GetKeyDown(KeyCode.Alpha1) && BaitMenuOpen)
+        {
+            if (FishBaitCount > 0)
+            {
+                // Load up UseBait Enum -> Decrement FishBaitCount -> Debug Log message confirmation
+                UseBait();
+                FishBaitCount--;
+                Debug.Log("You used a fish bait!");
+            }
+            else
+            {
+                Debug.Log("You don't have any fish baits!");
+            }
+        }
+
+        // INPUT CHECK 2 - Junk Bait
+        if (Input.GetKeyDown(KeyCode.Alpha2) && BaitMenuOpen)
+        {
+            if (JunkBaitCount > 0)
+            {
+                // Load up UseBait Enum -> Decrement JunkBaitCount -> Debug Log message confirmation
+                UseBait();
+                JunkBaitCount--;
+                Debug.Log("You used a junk bait!");
+            }
+            else
+            {
+                Debug.Log("You don't have any junk baits!");
+            }
+        }
+
+        // INPUT CHECK 3 - Treasure Bait
+        if (Input.GetKeyDown(KeyCode.Alpha3) && BaitMenuOpen)
+        {
+            if (TreasureBaitCount > 0)
+            {
+                // Load up UseBait Enum -> Decrement TreasureBaitCount -> Debug Log message confirmation
+                UseBait();
+                TreasureBaitCount--;
+                Debug.Log("You used a treasure bait!");
+            }
+            else
+            {
+                Debug.Log("You don't have any treasure baits!");
+            }
+        }
+
+        // INPUT CHECK 4 - Sea Monster Bait
+        if (Input.GetKeyDown(KeyCode.Alpha4) && BaitMenuOpen)
+        {
+            if (SeaMonsterBaitCount > 0)
+            {
+                // Load up UseBait Enum -> Decrement SeaMonsterBaitCount -> Debug Log message confirmation
+                UseBait();
+                SeaMonsterBaitCount--;
+                Debug.Log("You used a sea monster bait!");
+            }
+            else
+            {
+                Debug.Log("You don't have any sea monster baits!");
+            }
+        }
+
+        // INPUT CHECK 5 - Super AllRounder Bait
+        if (Input.GetKeyDown(KeyCode.Alpha5) && BaitMenuOpen)
+        {
+            if (SuperAllRounderBaitCount > 0)
+            {
+                // Load up UseBait Enum -> Decrement SuperAllRounderBaitCount -> Debug Log message confirmation
+                UseBait();
+                SuperAllRounderBaitCount--;
+                Debug.Log("You used a super all-rounder bait!");
+            }
+            else
+            {
+                Debug.Log("You don't have any super all-rounder baits!");
             }
         }
     }
